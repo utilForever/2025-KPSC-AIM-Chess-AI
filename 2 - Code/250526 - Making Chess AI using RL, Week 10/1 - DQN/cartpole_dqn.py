36650527d53da84f39d4bc48b5f9658c52707661
Chris Ohk <utilforever@gmail.com>
2025-05-26 23:32:49 +0900

    feat: Update an example code for Week 10

--- 2 - Code/250526 - Making Chess AI using RL, Week 10/1 - DQN/cartpole_dqn.py
+++ 2 - Code/250526 - Making Chess AI using RL, Week 10/1 - DQN/cartpole_dqn.py
@@ -1,176 +1,204 @@
-import sys
-import gym
-import pylab
+import gymnasium as gym
+import math
 import random
-import numpy as np
-from collections import deque
-
+import matplotlib.pyplot as plt
 import torch
-from torch import nn, optim
+import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 
-EPISODES = 300
+from collections import namedtuple, deque
+from itertools import count
 
 
-# 카트폴 예제에서의 DQN 에이전트
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.render = False
-        self.load_model = False
+env = gym.make("CartPole-v1")
 
-        # 상태와 행동의 크기 정의
-        self.state_size = state_size
-        self.action_size = action_size
+plt.ion()
 
-        # DQN 하이퍼파라미터
-        self.discount_factor = 0.99
-        self.learning_rate = 0.001
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.999
-        self.epsilon_min = 0.01
-        self.batch_size = 64
-        self.train_start = 1000
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # 리플레이 메모리, 최대 크기 2000
-        self.memory = deque(maxlen=2000)
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
-        # 모델과 타깃 모델 생성
-        self.model = self.build_model()
-        self.target_model = self.build_model()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
-        # 타깃 모델 초기화
-        self.update_target_model()
+class ReplayMemory(object):
 
-        if self.load_model:
-            self.model.load_state_dict(
-                torch.load("./save_model/cartpole_dqn_trained.bin")
-            )
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
 
-    # 상태가 입력, 큐함수가 출력인 인공신경망 생성
-    def build_model(self):
-        model = nn.Sequential(
-            nn.Linear(self.state_size, 24),
-            nn.ReLU(),
-            nn.Linear(24, 24),
-            nn.ReLU(),
-            nn.Linear(24, self.action_size),
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+n_actions = env.action_space.n
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
+        -1.0 * steps_done / EPS_DECAY
+    )
+    steps_done += 1
+
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor(
+            [[env.action_space.sample()]], device=device, dtype=torch.long
         )
-        return model
 
-    # 타깃 모델을 모델의 가중치로 업데이트
-    def update_target_model(self):
-        self.target_model.load_state_dict(self.model.state_dict())
 
-    # 입실론 탐욕 정책으로 행동 선택
-    def get_action(self, state):
-        if np.random.rand() <= self.epsilon:
-            # 무작위 행동 반환
-            return torch.LongTensor([[random.randrange(2)]])
+episode_durations = []
+
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+
+    if show_result:
+        plt.title("Result")
+    else:
+        plt.clf()
+        plt.title("Training...")
+
+    plt.xlabel("Episode")
+    plt.ylabel("Duration")
+    plt.plot(durations_t.numpy())
+
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(
+        tuple(map(lambda s: s is not None, batch.next_state)),
+        device=device,
+        dtype=torch.bool,
+    )
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+
+    with torch.no_grad():
+        next_state_values[non_final_mask] = (
+            target_net(non_final_next_states).max(1).values
+        )
+
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+
+if torch.cuda.is_available():
+    num_episodes = 600
+else:
+    num_episodes = 50
+
+for i_episode in range(num_episodes):
+    state, info = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
+
+        if terminated:
+            next_state = None
         else:
-            # 모델로부터 행동 산출
-            return self.model(state).data.max(1)[1].view(1, 1)
+            next_state = torch.tensor(
+                observation, dtype=torch.float32, device=device
+            ).unsqueeze(0)
 
-    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
-    def append_sample(self, state, action, reward, next_state, done):
-        reward = torch.FloatTensor([reward])
-        next_state = torch.FloatTensor([next_state])
-        done = torch.FloatTensor([done])
+        memory.push(state, action, next_state, reward)
 
-        self.memory.append((state, action, reward, next_state, done))
+        state = next_state
 
-    # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
-    def train_model(self):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        optimize_model()
 
-        # 메모리에서 배치 크기만큼 무작위로 샘플 추출
-        batch = random.sample(self.memory, self.batch_size)
-        states, actions, rewards, next_states, dones = zip(*batch)
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
 
-        states = torch.cat(states)
-        actions = torch.cat(actions)
-        rewards = torch.cat(rewards)
-        next_states = torch.cat(next_states)
-        dones = torch.cat(dones)
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[
+                key
+            ] * TAU + target_net_state_dict[key] * (1 - TAU)
 
-        # 현재 상태에 대한 모델의 큐함수
-        # 다음 상태에 대한 타깃 모델의 큐함수
-        current_q = self.model(states).gather(1, actions)
-        max_next_q = self.target_model(next_states).detach().max(1)[0]
-        expected_q = rewards + (self.discount_factor * max_next_q)
+        target_net.load_state_dict(target_net_state_dict)
 
-        # 벨만 최적 방정식을 이용한 업데이트 타깃
-        self.optimizer.zero_grad()
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
 
-        loss = F.mse_loss(current_q.squeeze(), expected_q)
-        loss.backward()
+print("Complete")
 
-        self.optimizer.step()
-
-
-if __name__ == "__main__":
-    # CartPole-v1 환경, 최대 타임스텝 수가 500
-    env = gym.make("CartPole-v1")
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-
-    # DQN 에이전트 생성
-    agent = DQNAgent(state_size, action_size)
-
-    scores, episodes = [], []
-
-    for e in range(EPISODES):
-        done = False
-        score = 0
-        # env 초기화
-        state = env.reset()
-
-        while not done:
-            # env.render()
-
-            # 현재 상태로 행동을 선택
-            state = torch.FloatTensor([state])
-            action = agent.get_action(state)
-            # 선택한 행동으로 환경에서 한 타임스텝 진행
-            next_state, reward, done, info = env.step(action.item())
-
-            # 에피소드가 중간에 끝나면 -100 보상
-            reward = reward if not done or score == 499 else -100
-
-            # 리플레이 메모리에 샘플 <s, a, r, s'> 저장
-            agent.append_sample(state, action, reward, next_state, done)
-            # 매 타임스텝마다 학습
-            if len(agent.memory) >= agent.train_start:
-                agent.train_model()
-
-            score += reward
-            state = next_state
-
-            if done:
-                # 각 에피소드마다 타깃 모델을 모델의 가중치로 업데이트
-                agent.update_target_model()
-
-                score = score if score == 500 else score + 100
-                # 에피소드마다 학습 결과 출력
-                scores.append(score)
-                episodes.append(e)
-                pylab.plot(episodes, scores, "b")
-                pylab.savefig("./save_graph/cartpole_dqn.png")
-                print(
-                    "episode:",
-                    e,
-                    "  score:",
-                    score,
-                    "  memory length:",
-                    len(agent.memory),
-                    "  epsilon:",
-                    agent.epsilon,
-                )
-
-                # 이전 10개 에피소드의 점수 평균이 490보다 크면 학습 중단
-                if np.mean(scores[-min(10, len(scores)) :]) > 490:
-                    torch.save(
-                        agent.model.state_dict(), "./save_model/cartpole_dqn.bin"
-                    )
-                    sys.exit()
+plot_durations(show_result=True)
+plt.ioff()
+plt.show()
